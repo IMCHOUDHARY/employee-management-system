5a6d5f8c970e278c3eecdfb2801cda02b29f2b1d
IMCHOUDHARY <36573566+IMCHOUDHARY@users.noreply.github.com>
2023-09-15 13:31:49 +0530

    Add files via upload

---
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -0,0 +1,65 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {AuthService} from "../service/auth.service";
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-emp-add-edit',
+  templateUrl: './emp-add-edit.component.html',
+  styleUrls: ['./emp-add-edit.component.css']
+})
+export class EmpAddEditComponent implements OnInit{
+  empForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private service: AuthService,
+    private dialogRef: MatDialogRef<EmpAddEditComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private toastr: ToastrService
+  ) {
+    this.empForm = this.fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      dob: '',
+      gender: '',
+      department: '',
+      experience: '',
+    });
+  }
+
+  ngOnInit(): void {
+    this.empForm.patchValue(this.data);
+  }
+
+  onFormSubmit() {
+    if (this.empForm.valid) {
+      if (this.data) {
+        this.service
+          .updateEmployee(this.data.id, this.empForm.value)
+          .subscribe({
+            next: (val: any) => {
+              this.dialogRef.close(true);
+              this.toastr.success('Employee detail updated!');
+            },
+            error: (err: any) => {
+              console.error(err);
+              this.toastr.error('Failed to update Employee details!');
+            },
+          });
+      } else {
+        this.service.addEmployee(this.empForm.value).subscribe({
+          next: (val: any) => {
+            this.toastr.success('Employee added successfully');
+            this.dialogRef.close(true);
+          },
+          error: (err: any) => {
+            console.error(err);
+          },
+        });
+      }
+    }
+  }
+}
