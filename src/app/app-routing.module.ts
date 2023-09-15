4d86b90b434aef2433396e782f2e19ccf35ba4d2
ankit.choudhary <ankit.choudhary@vista.com>
2023-09-15 13:24:24 +0530

    Contains all the files.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeComponent } from './employee/employee.component';
+import { AuthGuard } from './guard/auth.guard';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
+import {ForgotPasswordComponent} from "./forgot-password/forgot-password.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {component:LoginComponent,path:'login'},
+  {component:RegisterComponent,path:'register'},
+  {component:ForgotPasswordComponent,path:'forgot-password'},
+  {component:HomeComponent,path:'',canActivate:[AuthGuard]},
+  {component:UserComponent,path:'user',canActivate:[AuthGuard]},
+  {component:EmployeeComponent,path:'employee',canActivate:[AuthGuard]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,52 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MaterialModule} from 'src/material.module';
+import {HttpClientModule} from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { UpdatepopupComponent } from './updatepopup/updatepopup.component';
+import { EmployeeComponent } from './employee/employee.component';
+import {MatIconModule} from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    UserComponent,
+    UpdatepopupComponent,
+    EmployeeComponent,
+    EmpAddEditComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    MatIconModule,
+    MatDialogModule,
+    HttpClientModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
